f80f654c5fdce85fa4b991ebee543c35e2286b6c
Luca Sartore <lucasar.com8@gmail.com>
2024-05-05 11:47:22 +0200

    add hello world backend

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+import { MongoClient, ServerApiVersion } from "mongodb";
+import app from "./app/app.js";
+
 
 // connect to MongoDB
 const client = new MongoClient(process.env.DB_URL, {
@@ -9,18 +11,45 @@ const client = new MongoClient(process.env.DB_URL, {
   }
 });
 
-async function connectDb(){
-    try { await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Connected successfully to MongoDB server");
-    } catch (e) {
-        console.error("failed to connect to MongoDB because of error: "+e);
-        await client.close();
-        process.exit(1);
-    }
+const port = process.env.PORT || 8080;
+
+async function connectDb() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to MongoDB server");
+  } catch (e) {
+    console.error("failed to connect to MongoDB because of error: " + e);
+    await closeDb();
+    process.exit(1);
+  }
+}
+
+async function launchServer() {
+  try {
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  } catch (e) {
+    console.error("failed to execute server because of error: " + e);
+    await closeDb();
+    process.exit(1);
+  }
+}
+
+async function closeDb() {
+  try {
+    await client.close();
+  } catch (e) {
+    console.error("failed to close database because of error: " + e);
+    process.exit(1);
+  }
 }
 
 
-connectDb().then(() => {
-    process.exit(0);
-});
+async function main() {
+  await connectDb();
+  await launchServer();
+}
+
+main();
