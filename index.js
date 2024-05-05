40bad4be63422f5deec3c9220803835caf77762e
Luca Sartore <lucasar.com8@gmail.com>
2024-05-05 11:02:05 +0200

    minor fixes

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ const client = new MongoClient(process.env.DB_URL, {
   }
 });
 
-async function connect(){
+async function connectDb(){
     try { await client.connect();
         await client.db("admin").command({ ping: 1 });
         console.log("Connected successfully to MongoDB server");
     } catch (e) {
-        console.error(e);
+        console.error("failed to connect to MongoDB because of error: "+e);
         await client.close();
         process.exit(1);
     }
 }
 
 
-connect().then(() => {
-
+connectDb().then(() => {
+    process.exit(0);
 });
