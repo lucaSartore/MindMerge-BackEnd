97576d48a85d9e4b5730c53bc4008e554377ad89
Gabriele Benetti~ <gabriele.benetti7@gmail.com>
2024-05-13 12:34:31 +0200

    Add mongoose schema for user

--- app/database_manager/database_manager.js
+++ app/database_manager/database_manager.js
@@ -4,6 +4,14 @@ const Task = mongoose.model("Task", {
     taskId: Number,
 });
 
+const User = mongoose.model("User", {
+    userId: Number,
+    userName: String,
+    organizations: [Number],
+    userKind: Number,
+    email: String,
+});
+
 /**
  * @typedef dataBaseManager
  * @type {Object}
