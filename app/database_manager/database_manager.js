4d665cb90f1fb4b3bb78e3040d79e01b7fafc1e7
Luca Sartore <lucasar.com8@gmail.com>
2024-05-13 13:13:35 +0200

    Add TaskSchema mongoose

--- app/database_manager/database_manager.js
+++ app/database_manager/database_manager.js
@@ -1,8 +1,27 @@
 import mongoose from "mongoose";
 
-const Task = mongoose.model("Task", {
-    taskId: Number,
+const TaskReportScheduleSchema = new mongoose.Schema({
+    // todo for Gioele
 });
+
+const TaskSchema = new mongoose.Schema({
+    taskId: {Number, required: true, unique: true},
+    taskFatherId: Number,
+    lastUpdated: {type: Date, default: Date.now, required: true},
+    taskName: {type: String, required: true},
+    taskDescription: {type: String, required: true},
+    taskStatus: {type: Number, required: true},
+    taskNotes: {type: [TaskNote], required: true, default: []},
+    taskAssignees: {type: [Number], required: true, default: []},
+    taskManager: {type: Number, required: true},
+    taskOrganizationId: {type: Number, required: true},
+    taskReports: {type: [TaskReportScheduleSchema], required: true, default: []},
+    notificationEnable: {type: Boolean, required: true, default: false},
+    childTasks: {type: [Number], required: true, default: []},
+    recusivePermissionsValue: {type: Number, required: true}
+});
+
+const TaskModel = mongoose.model("Task", TaskSchema);
 
 /**
  * @typedef dataBaseManager
