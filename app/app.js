ab876a89b602f46f42adcbe7ff0e39a8bb6ce035
gioele-bernardini <gioele.g@outlook.com>
2024-06-19 19:23:06 +0200

    middleware updated

--- app/app.js
+++ app/app.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
                 res.status(403).json({success:false, message: 'Invalid token'})
             else {
                 req.loggedUser = decoded;
+                var organization_id = req.body.organization_id || 
                 next();
             }
         }
