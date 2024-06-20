a768d30afe97d6418c54aa5197eb36529f8d8203
gioele-bernardini <gioele.g@outlook.com>
2024-06-20 10:35:56 +0200

    finish middleware to check user token

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const {accountRouter, userRouter} = require('./services/account_manager/account_manager.js');
+const {accountRouter, userRouter, accountManager} = require('./services/account_manager/account_manager.js');
 const {organizationEditorRouter} = require('./services/organization_manager/organization_editor.js');
 const { Errors } = require('./common_infrastructure/errors.js');
 const {testingRouter} = require('./services/notification_manager/external_norification_manager.js');
 const {taskRouter} = require('./services/task_manager/task_router.js');
-
 const app = express();
 
 app.use(express.json());
@@ -25,7 +24,7 @@ app.get('/hello', async (req, res) => {
 app.use('/api/v1/account', accountRouter);
 
 // Middleware to verify the user token
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
     var token = req.body.token || req.query.token || req.headers['token'];
 
     if (!token) res.status(403).json({success:false, message: 'No token provided.'})
@@ -33,11 +32,28 @@ app.use((req, res, next) => {
     jwt.verify (
         token,
         process.env.SUPER_SECRET,
-        function(err, decoded) {
+        async function(err, decoded) {
             if (err)
                 res.status(403).json({success:false, message: 'Invalid token'})
             else {
-                req.loggedUser = decoded;
+                req.loggedUser = decoded.userId * 1;
+                let organizationId = req.params.organization_id || req.query.organization_id;
+                
+                if (!organizationId){
+                    next();
+                    return;
+                }
+                organizationId = organizationId * 1;
+                
+                let isInOrganization = await accountManager.verifyUserIsInOrganization(req.loggedUser, organizationId)
+                if (isInOrganization.statusCode != Errors.OK){
+                    res.status(isInOrganization.statusCode).json(isInOrganization);
+                    return;
+                }
+                if (!isInOrganization.payload){
+                    res.status(403).json({success:false, message: 'User has no access to the requested organization'});
+                    return;
+                }
                 // TODO: match organization_id with user request
                 next();
             }
