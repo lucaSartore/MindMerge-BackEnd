75935255fd0cd56c81dfa152152d2120f5b8c670
gioele-bernardini <gioele.g@outlook.com>
2024-06-20 10:53:28 +0200

    move autentication middleware out of app.js file

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const {accountRouter, userRouter, accountManager} = require('./services/account_manager/account_manager.js');
+const {accountRouter, userRouter} = require('./services/account_manager/account_manager.js');
 const {organizationEditorRouter} = require('./services/organization_manager/organization_editor.js');
-const { Errors } = require('./common_infrastructure/errors.js');
 const {testingRouter} = require('./services/notification_manager/external_norification_manager.js');
 const {taskRouter} = require('./services/task_manager/task_router.js');
 const app = express();
+const {authenticationMiddleware} = require('./middleware/authentication_middleware.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,42 +23,7 @@ app.get('/hello', async (req, res) => {
 app.use('/api/v1/account', accountRouter);
 
 // Middleware to verify the user token
-app.use(async (req, res, next) => {
-    var token = req.body.token || req.query.token || req.headers['token'];
-
-    if (!token) res.status(403).json({success:false, message: 'No token provided.'})
-
-    jwt.verify (
-        token,
-        process.env.SUPER_SECRET,
-        async function(err, decoded) {
-            if (err)
-                res.status(403).json({success:false, message: 'Invalid token'})
-            else {
-                req.loggedUser = decoded.userId * 1;
-                let organizationId = req.params.organization_id || req.query.organization_id;
-                
-                if (!organizationId){
-                    next();
-                    return;
-                }
-                organizationId = organizationId * 1;
-                
-                let isInOrganization = await accountManager.verifyUserIsInOrganization(req.loggedUser, organizationId)
-                if (isInOrganization.statusCode != Errors.OK){
-                    res.status(isInOrganization.statusCode).json(isInOrganization);
-                    return;
-                }
-                if (!isInOrganization.payload){
-                    res.status(403).json({success:false, message: 'User has no access to the requested organization'});
-                    return;
-                }
-                // TODO: match organization_id with user request
-                next();
-            }
-        }
-    )
-});
+app.use(authenticationMiddleware);
 
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/organization', organizationEditorRouter);
---
+++ app/middleware/authentication_middleware.js
@@ -0,0 +1,42 @@
+const { Errors } = require('../common_infrastructure/errors.js');
+const jwt = require('jsonwebtoken');
+const {accountManager} = require('../services/account_manager/account_manager.js');
+
+async function authenticationMiddleware(req, res, next)  {
+    var token = req.body.token || req.query.token || req.headers['token'];
+
+    if (!token) res.status(403).json({success:false, message: 'No token provided.'})
+
+    jwt.verify (
+        token,
+        process.env.SUPER_SECRET,
+        async function(err, decoded) {
+            if (err)
+                res.status(403).json({success:false, message: 'Invalid token'})
+            else {
+                req.loggedUser = decoded.userId * 1;
+                let organizationId = req.params.organization_id || req.query.organization_id;
+                
+                if (!organizationId){
+                    next();
+                    return;
+                }
+                organizationId = organizationId * 1;
+                
+                let isInOrganization = await accountManager.verifyUserIsInOrganization(req.loggedUser, organizationId)
+                if (isInOrganization.statusCode != Errors.OK){
+                    res.status(isInOrganization.statusCode).json(isInOrganization);
+                    return;
+                }
+                if (!isInOrganization.payload){
+                    res.status(403).json({success:false, message: 'User has no access to the requested organization'});
+                    return;
+                }
+                // TODO: match organization_id with user request
+                next();
+            }
+        }
+    )
+}
+
+exports.authenticationMiddleware = authenticationMiddleware;
