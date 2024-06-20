328a8ca5ab25abbfa4e1dac27d8bc3e05672a781
Gabriele Benetti <gabriele.benetti7@gmail.com>
2024-06-20 14:42:30 +0200

    handle error for task router calls

--- app/services/task_manager/task_router.js
+++ app/services/task_manager/task_router.js
@@ -5,20 +5,20 @@ const { taskCreator} = require('./task_creator');
 const { taskEditor } = require('./task_editor.js'); 
 const { TaskNote } = require('../../common_infrastructure/task_note.js');
 const taskRouter = express.Router();
+const {requestWrapper} = require('../../middleware/global_error_handler_middleware.js')
 
 
-taskRouter.get('/task_tree', async (req, res) => {
+taskRouter.get('/task_tree',requestWrapper( async (req, res) => {
 
     const userId = req.query.user_id * 1;
     const organizationId = req.query.organization_id * 1;
-
     let response = await taskGetter.getTasksForUser(organizationId, userId);
     res.status(response.statusCode);
     res.json(response);
-});
+}));
 
 
-taskRouter.get('/:task_id', async (req, res) => {
+taskRouter.get('/:task_id',requestWrapper( async (req, res) => {
 
     const taskId = req.params.task_id * 1;
     const organizationId = req.query.organization_id * 1;
@@ -26,11 +26,11 @@ taskRouter.get('/:task_id', async (req, res) => {
     let response = await taskGetter.getTask(organizationId, taskId,null,null);
     res.status(response.statusCode);
     res.json(response);
-});
+}));
 
 
 
-taskRouter.put("/:task_id/name/:new_name", async (req,res) => {
+taskRouter.put("/:task_id/name/:new_name",requestWrapper( async (req,res) => {
 
     const taskId = req.params.task_id*1;
     const organizationId = req.query.organization_id * 1;
@@ -39,9 +39,9 @@ taskRouter.put("/:task_id/name/:new_name", async (req,res) => {
     let response = await taskEditor.updateTaskName(organizationId,taskId,newName,null,null);
     res.status(response.statusCode);
     res.json(response);
-})
+}))
 
-taskRouter.put("/:task_id/notes/:notes_id", async (req,res) => {
+taskRouter.put("/:task_id/notes/:notes_id",requestWrapper( async (req,res) => {
         const taskId = req.params.task_id * 1;
         const organizationId = req.query.organization_id * 1;
         const noteId = req.params.notes_id * 1;
@@ -49,9 +49,9 @@ taskRouter.put("/:task_id/notes/:notes_id", async (req,res) => {
         let response = await taskEditor.updateTaskNotes(organizationId,taskId,noteId,newNotes,null,null);
         res.status(response.statusCode);
         res.json(response);
-});
+}));
 
-taskRouter.post("/", async (req,res) =>{
+taskRouter.post("/",requestWrapper( async (req,res) =>{
 
     let task = req.body;
     task = Object.assign(new Task(), task);
@@ -60,35 +60,35 @@ taskRouter.post("/", async (req,res) =>{
 
     res.status(response.statusCode);
     res.json(response);
-});
+}));
 
-taskRouter.post("/:task_id/notes/", async (req,res) => {
+taskRouter.post("/:task_id/notes/",requestWrapper( async (req,res) => {
     const organizationId = req.query.organization_id * 1;
     let taskNote = req.body;
     taskNote = Object.assign(new TaskNote(), taskNote);
     let response = await taskEditor.createTaskNotes(organizationId,taskNote.taskId,taskNote.notes);
     res.status(response.statusCode);
     res.json(response);
-});
+}));
 
-taskRouter.delete("/:task_id", async (req,res) => {
+taskRouter.delete("/:task_id",requestWrapper( async (req,res) => {
     const organizationId = req.query.organization_id * 1;
     const taskId = req.params.task_id * 1;
     let response = await taskEditor.deleteTask(organizationId,taskId,undefined,undefined);    
     res.status(response.statusCode);
     res.json(response);
-});
+}));
 
-taskRouter.delete("/:task_id/notes/:note_id", async (req,res) => {
+taskRouter.delete("/:task_id/notes/:note_id",requestWrapper( async (req,res) => {
     const organizationId = req.query.organization_id * 1;
     const taskId = req.params.task_id * 1;
     const noteId = req.params.note_id * 1;
     let response = await taskEditor.deleteTaskNotes(organizationId,taskId,noteId);
     res.status(response.statusCode);
     res.json(response);
-});
+}));
 
-taskRouter.post("/:task_id/assignee/:assignee_id", async (req,res) => {
+taskRouter.post("/:task_id/assignee/:assignee_id",requestWrapper( async (req,res) => {
     const organizationId = req.query.organization_id * 1;
     const taskId = req.params.task_id * 1;
     const assigneeId = req.params.assignee_id * 1;
@@ -96,10 +96,10 @@ taskRouter.post("/:task_id/assignee/:assignee_id", async (req,res) => {
     let response = await taskEditor.addNewAssignee(organizationId,taskId,assigneeId,null,null);
     res.status(response.statusCode);
     res.json(response);
-});
+}));
 
 
-taskRouter.delete("/:task_id/assignee/:assignee_id", async (req,res) => {
+taskRouter.delete("/:task_id/assignee/:assignee_id",requestWrapper( async (req,res) => {
     const organizationId = req.query.organization_id * 1;
     const taskId = req.params.task_id * 1;
     const assigneeId = req.params.assignee_id * 1;
@@ -107,9 +107,9 @@ taskRouter.delete("/:task_id/assignee/:assignee_id", async (req,res) => {
     let response = await taskEditor.deleteTaskAssignee(organizationId,taskId,assigneeId,null,null);
     res.status(response.statusCode);
     res.json(response);
-});
+}));
 
-taskRouter.put("/:task_id/status/:new_status", async (req,res) => {
+taskRouter.put("/:task_id/status/:new_status",requestWrapper( async (req,res) => {
     const organizationId = req.query.organization_id * 1;
     const taskId = req.params.task_id * 1;
     const newStatus = req.params.new_status * 1;
@@ -117,6 +117,6 @@ taskRouter.put("/:task_id/status/:new_status", async (req,res) => {
     let response = await taskEditor.updateTaskStatus(organizationId,taskId,newStatus,null,null);
     res.status(response.statusCode);
     res.json(response);
-});
+}));
 
 exports.taskRouter = taskRouter;
