b548807f5f623013a69634661711ccc42c8f3f59
gioele-bernardini <gioele.g@outlook.com>
2024-06-20 11:17:27 +0200

    test render error 3

--- app/middleware/authentication_middleware.js
+++ app/middleware/authentication_middleware.js
@@ -5,15 +5,18 @@ const {accountManager} = require('../services/account_manager/account_manager.js
 async function authenticationMiddleware(req, res, next)  {
     var token = req.body.token || req.query.token || req.headers['token'];
 
-    if (!token) res.status(403).json({success:false, message: 'No token provided.'})
+    if (!token){
+        res.status(403);
+        res.json({success:false, message: 'No token provided.'});
+    }
 
     jwt.verify (
         token,
         process.env.SUPER_SECRET,
         async function(err, decoded) {
             if (err){
-                res.json({success:false, message: 'Invalid token'})
                 res.status(403);
+                res.json({success:false, message: 'Invalid token'})
                 return;
             }
             else {
@@ -28,16 +31,16 @@ async function authenticationMiddleware(req, res, next)  {
                 
                 let isInOrganization = await accountManager.verifyUserIsInOrganization(req.loggedUser, organizationId)
                 if (isInOrganization.statusCode != Errors.OK){
-                    res.json(isInOrganization);
                     res.status(isInOrganization.statusCode);
+                    res.json(isInOrganization);
                     return;
                 }
                 if (!isInOrganization.payload){
-                    res.json({success:false, message: 'User has no access to the requested organization'});
                     res.status(403);
+                    res.json({success:false, message: 'User has no access to the requested organization'});
                     return;
                 }
-                
+
                 next();
             }
         }
